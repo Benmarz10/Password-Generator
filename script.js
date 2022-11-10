4c30da96682b79928cc44d9986d3af2f31f0cd51
Benmarz10 <benjaminmarz10@gmail.com>
2022-11-10 14:20:52 -0600

    Added more var and functions to generate acceptable password

--- script.js
+++ script.js
@@ -4,6 +4,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function randomInt(min, max) {
+  if (!max) {
+    max = min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1 - rand) + rand*max)
+}
+
+function getRandomChar(list) {
+  return list[randomInt(list.length)]
+}
+
 function generatePassword() {
  var userInput = window.prompt("How long of a password would you like?")
  var passLength = parseInt(userInput)
@@ -21,6 +34,37 @@ function generatePassword() {
   var userOksSymbols = window.confirm("Do you want symbols in your password?")
   var userOksUppercase = window.confirm("Do you want uppercase letters in your password?")
   var userOksLowercase = window.confirm("Do you want lowercase letters in your password?")
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "=", "_", "+"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  var options = []
+
+  if (userOksNumbers === true) {
+    options.push(numberList)
+  }
+
+  if (userOksSymbols === true) {
+    options.push(symbolList)
+  }
+
+  if (userOksLowercase === true) {
+    options.push(lowercaseList)
+  }
+
+  if (userOksUppercase === true) {
+    options.push(uppercaseList)
+  }
+
+  var generatePassword = ""
+
+  for (var i = 0; i < passLength; i++) {
+    var randomList = getRandomChar(options)
+    var randomItem = getRandomChar(randomList)
+    generatePassword += randomItem
+  }
 }
 
 
